ae5dbdb83b4c3bc12e5eb187166aa18fd1463a97
xNanoEngine <xnanoengine@gmail.com>
2023-05-04 12:45:42 -0400

    componente loading

--- frontend/src/pages/Concierto.jsx
+++ frontend/src/pages/Concierto.jsx
@@ -2,23 +2,25 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import Formulario from "../components/Formulario";
 import { data } from "autoprefixer";
-import Home from "./Home";
+import Loading from "./Loading";
 import axios from "axios";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 function Concierto() {
   const [data, setData] = useState("NO");
 
   useEffect(() => {
-    const cookies = document.cookie.split(';');
+    const cookies = document.cookie.split(";");
     // Busca la cookie con el nombre "token"
-    const tokenCookie = cookies.find(cookie => cookie.trim().startsWith('token='));
+    const tokenCookie = cookies.find((cookie) =>
+      cookie.trim().startsWith("token=")
+    );
     // Si se encuentra la cookie, extrae el valor del token
     let token = null;
     if (tokenCookie) {
-      token = tokenCookie.split('=')[1];
+      token = tokenCookie.split("=")[1];
     }
-    
+
     const intervalId = setInterval(() => {
       axios
         .put("http://127.0.0.1:4000/token", {
@@ -39,7 +41,7 @@ function Concierto() {
 
   return (
     <div className="mt-5 p-5">
-      {data === "SI" ? <Formulario />: <h1>COLA</h1>}
+      {data === "SI" ? <Formulario /> : <Loading />}
     </div>
   );
 }
